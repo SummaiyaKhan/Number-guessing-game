b27f2afaa8f97819cee99a4414e501279ede70b1
SummaiyaKhan98 <nk4937169@gmail.com SummaiyaKhan98>
2024-06-25 06:24:49 -0700

    push number guessing game on github

---
+++ index.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// 1) computer will genrate a random number;
+// 2) user input for guessing number;
+// 3) compare user input with computer gerated number and show result;
+// const randomNumber = 13;
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "please guess a number between 1-10:",
+    }
+]);
+if (answers.userGuessNumber === randomNumber) {
+    console.log("Congratulations! you are guessed right number");
+}
+else {
+    console.log("you guessed worng number");
+}
